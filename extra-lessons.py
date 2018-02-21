52e9ee694bb245630fb4842addce0ea3043c5665
captain-c00keys <gpieterson.wsu@hotmail.com>
2018-02-21 14:38:18 -0800

    adding new repo

---
+++ extra-lessons.py
@@ -0,0 +1,5 @@
+#formatting
+#putting {} then .format will place anything after in () inside of the curtly brackets through the format method
+print('This is a string {}'.format('INSERT'))
+print('The {} {} {}'.format('fox', 'brown', 'quick'))
+print('The {2} {1} {0}'.format('fox', 'brown', 'quick')) #this will reverse the order of the words because of the indexes placed inside of the curly brackets
