18652d39352cc7a67ccebbe6fbe2a2bd644804d8
captain-c00keys <gpieterson.wsu@hotmail.com>
2018-02-21 16:02:20 -0800

    finishing up with format method

--- extra-lessons.py
+++ extra-lessons.py
@@ -9,3 +9,17 @@ print('The {2} {1} {0}'.format('fox', 'brown', 'quick'))
 
 #you can assign the strings inside of the format and place those variables inside of the curly brackets
 print('The {q} {b} {f}'.format(f='fox', b='brown', q='quick'))
+
+result = 100/888
+
+#{r:1.5f} the r is the variable inside of format, the 1 is the amount of whitespace, and the 3f is how many places
+# after the period
+print("The result was {r:1.3f}".format(r=result))
+
+#ex
+
+name = "jose"
+print('Hello, his name is {}'.format(name))
+
+#displays the same as above. It's called f string literals
+print(f'Hello, his name is {name}')
