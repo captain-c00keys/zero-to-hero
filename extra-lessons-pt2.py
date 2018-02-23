20550403d1a542aa833301564b1d77b9190f1868
captain-c00keys <gpieterson.wsu@hotmail.com>
2018-02-23 12:37:01 -0800

    added new lesson

---
+++ extra-lessons-pt2.py
@@ -0,0 +1,7 @@
+myset = set() #will reutrn set() when called
+myset.add(1) #will add 1 to the set as {1}
+
+#sets only add unique values
+
+mylist = [1,1,1,1,1,1,2,2,2,2,3,3,3,3,3]
+set(mylist) #returns {1,2,3}
