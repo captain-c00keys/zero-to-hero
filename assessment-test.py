7890a83fb2c64b63c3f8498daebd9faaa400c53b
captain-c00keys <gpieterson.wsu@hotmail.com>
2018-02-25 22:01:16 -0800

    tuples

--- assessment-test.py
+++ assessment-test.py
@@ -1,2 +1,4 @@
 #numbers are either integers or floats
-#strings are put within single quotes and can be captured at each index 
+#strings are put within single quotes and can be captured at each index
+#lists are in other words arrays, also are within square brackets
+#tuples are within parenthesis
