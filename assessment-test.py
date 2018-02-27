b1466e0ce2dd36b35cff9351937c4df00fa7f968
captain-c00keys <gpieterson.wsu@hotmail.com>
2018-02-26 21:45:50 -0800

    writing a few things about numbers

--- assessment-test.py
+++ assessment-test.py
@@ -2,3 +2,5 @@
 #strings are put within single quotes and can be captured at each index
 #lists are in other words arrays, also are within square brackets
 #tuples are within parenthesis
+#dictionaries have key and value paired components. Work kind of like an object in javascript
+
