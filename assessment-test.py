d78d61e5dc016a5325604068f9df9db5c239fd8f
captain-c00keys <gpieterson.wsu@hotmail.com>
2018-03-06 05:14:04 -0800

    adding a few things

--- assessment-test.py
+++ assessment-test.py
@@ -8,3 +8,7 @@
 >>> type(3+1.5+4)
 <type 'float'>
 >>>
+
+#square is ** : 4**2 = 16
+#square root 100 ** 0.5
+#build [0,0,0] => methond 1 : [0]*3 = [0,0,0] and method 2 : list2 = [0,0,0], then call list2
