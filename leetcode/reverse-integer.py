40222a00e498bf11b4901f6060f732e07247bdbf
captain-c00keys <gpieterson.wsu@hotmail.com>
2018-06-20 15:26:48 -0700

    few changes

--- leetcode/reverse-integer.py
+++ leetcode/reverse-integer.py
@@ -3,22 +3,13 @@
 
 def reverse(x):
     """Reverse input X."""
-    ab_temp = abs(x)
-    if x:
-        collection = []
-        while x:
-            digit = x % 10
-            x //= 10
-            collection.append(digit)
-        return collection
-
-    if -x:
-        collection = []
-        while ab_temp:
-            digit = ab_temp % 10
-            ab_temp //= 10
-            collection.append(digit)
-        return collection
+    temp = x
+    collection = []
+    while temp:
+        digit = temp % 10
+        temp //= 10
+        collection.append(digit)
+    return ','.join(collection)
 
 
 print(reverse(8738))
